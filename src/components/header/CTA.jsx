018861e3049ba4f7c9dda4ee1b1e8ffb120bac5b
shubham-am-i <patilshubham2k21@gmail.com>
2022-07-28 12:07:17 +0530

    add resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,14 @@
-import CV from '../../assets/cv.pdf'
-
+import CV from '../../assets/resume.pdf'
 
 export default function CTA() {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download Resume </a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <a href={CV} download className='btn'>
+        Download Resume{' '}
+      </a>
+      <a href='#contact' className='btn btn-primary'>
+        Let's Talk
+      </a>
     </div>
   )
 }
