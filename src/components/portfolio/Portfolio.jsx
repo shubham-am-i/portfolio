dd0efb41448f4c35e669490e3a53f554413e0b6e
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-27 22:42:41 +0530

    update new deployment links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,87 +1,84 @@
-import "./portfolio.css";
-import IMG1 from "../../assets/proshop.png";
-import IMG2 from "../../assets/habit_tracker3.png";
-import IMG3 from "../../assets/music_player.png";
-
+import './portfolio.css'
+import IMG1 from '../../assets/proshop.png'
+import IMG2 from '../../assets/habit_tracker3.png'
+import IMG3 from '../../assets/music_player.png'
 
 export default function Portfolio() {
   return (
-    <section id="portfolio">
+    <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt='' />
           </div>
           <h3>Proshop eCommerce App </h3>
-          <div className="portfolio__item-cta">
+          <div className='portfolio__item-cta'>
             <a
-              href="https://github.com/shubham-am-i/techshop"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
+              href='https://github.com/shubham-am-i/proshop'
+              className='btn'
+              target='_blank'
+              rel='noreferrer'
             >
               Github
             </a>
             <a
-              href="https://ecomm-proshop-app.herokuapp.com/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
+              href='https://proshop-btyz.onrender.com/'
+              className='btn btn-primary'
+              target='_blank'
+              rel='noreferrer'
             >
               Live Demo
             </a>
           </div>
         </article>
 
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt='' />
           </div>
           <h3>Music Player - Frontend </h3>
-          <div className="portfolio__item-cta">
+          <div className='portfolio__item-cta'>
             <a
-              href="https://github.com/shubham-am-i/inner-Vibes-Music-App"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
+              href='https://github.com/shubham-am-i/inner-Vibes-Music-App'
+              className='btn'
+              target='_blank'
+              rel='noreferrer'
             >
               Github
             </a>
             <a
-              href="https://inner-vibes-music-app.herokuapp.com/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
+              href='https://inner-vibes.netlify.app/'
+              className='btn btn-primary'
+              target='_blank'
+              rel='noreferrer'
             >
               Live Demo
             </a>
           </div>
         </article>
 
-        
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt='' />
           </div>
           <h3>Habit Tracker - Mini Project</h3>
-          <div className="portfolio__item-cta">
+          <div className='portfolio__item-cta'>
             <a
-              href="https://github.com/shubham-am-i/habit-tracker"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
+              href='https://github.com/shubham-am-i/habit-tracker'
+              className='btn'
+              target='_blank'
+              rel='noreferrer'
             >
               Github
             </a>
             <a
-              href="https://loop-habit-tracker.netlify.app/"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
+              href='https://loop-habit-tracker.netlify.app/'
+              className='btn btn-primary'
+              target='_blank'
+              rel='noreferrer'
             >
               Live Demo
             </a>
@@ -89,5 +86,5 @@ export default function Portfolio() {
         </article>
       </div>
     </section>
-  );
+  )
 }
